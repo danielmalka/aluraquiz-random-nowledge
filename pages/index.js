2d6bc96439f1e3717bef5c51bb88ce262b1c9912
Malka <contatolemos@hotmail.com>
2021-01-28 17:45:34 -0300

    Adicionando api/db e passando eslint

--- pages/index.js
+++ pages/index.js
@@ -28,47 +28,47 @@ export default function Home() {
   const [name, setName] = React.useState('');
 
   return (
-      <QuizBackground backgroundImage={db.bg}>
-        <Head>
-          <title>{db.title}</title>
-        </Head>
-        <QuizContainer>
-          <QuizLogo />
-          <Widget>
-            <Widget.Header>
-              <h1>{db.title}</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>{db.description}</p>
-              <form onSubmit={function (infosDoEvento) {
-                infosDoEvento.preventDefault();
-                router.push(`/quiz?name=${name}`);
-                console.log('Fazendo uma submissão por meio do react');
-              }}
-              >
-                <Input
-                    name="nomeDoUsuario"
-                    onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
-                    placeholder="Diz ai seu nome"
-                    value={name}
-                />
-                <Button type="submit" disabled={name.length === 0}>
-                  {`Jogar ${name}`}
-                </Button>
-              </form>
-            </Widget.Content>
-          </Widget>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
 
-          <Widget>
-            <Widget.Content>
-              <h1>Quizes da Galera</h1>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
 
-              <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
-        </QuizContainer>
-        <GitHubCorner projectUrl="https://github.com/danielmalka/aluraquiz-rpg" />
-      </QuizBackground>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/danielmalka/aluraquiz-rpg" />
+    </QuizBackground>
   );
 }
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import db from '../db.json';
 
 const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.secondary};
