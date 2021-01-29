e99be6e2f30a58ac4c957b82b974f6aa382f918a
Malka <contatolemos@hotmail.com>
2021-01-28 21:56:22 -0300

    Alterando Quiz para a aula 4

--- pages/quiz.js
+++ pages/quiz.js
@@ -8,141 +8,166 @@ import QuizContainer from '../src/components/QuizContainer';
 import Button from '../src/components/Button';
 
 function LoadingCard() {
-  return (
-    <Card>
-      <Card.Header>
-        Carregando...
-      </Card.Header>
+    return (
+        <Card>
+            <Card.Header>
+                Carregando...
+            </Card.Header>
 
-      <Card.Content>
-          <img
-              src={` ${db.path}/img/loading.gif`}
-              alt="Carregando"
-          />
-      </Card.Content>
-    </Card>
-  );
+            <Card.Content>
+                <img
+                    src={` ${db.path}/img/loading.gif`}
+                    alt="Carregando"
+                />
+            </Card.Content>
+        </Card>
+    );
 }
 
 function QuestionCard({
-  question,
-  questionIndex,
-  totalQuestions,
-  onSubmit,
-}) {
-  const questionId = `question__${questionIndex}`;
-  return (
-    <Card>
-      <Card.Header>
-        {/* <BackLinkArrow href="/" /> */}
-        <h3>
-          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
-        </h3>
-      </Card.Header>
+                          question,
+                          questionIndex,
+                          totalQuestions,
+                          onSubmit,
+                      }) {
+    const [selectedAlternative, setSelectedAlternative] = React.useState(undefined);
+    const [isQuestionSubmited, setIsQuestionSubmited] = React.useState(false);
+    const questionId = `question__${questionIndex}`;
+    const isCorrect = selectedAlternative === question.answer;
+    const hasAlternativeSelected = selectedAlternative !== undefined;
 
-      <img
-        alt="Descrição"
-        style={{
-          width: '100%',
-          height: '150px',
-          objectFit: 'cover',
-        }}
-        src={question.image}
-      />
-      <Card.Content>
-        <h2>
-          {question.title}
-        </h2>
-        <p>
-          {question.description}
-        </p>
+    return (
+        <Card>
+            <Card.Header>
+                {/* <BackLinkArrow href="/" /> */}
+                <h3>
+                    {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+                </h3>
+            </Card.Header>
 
-        <form
-          onSubmit={(infosDoEvento) => {
-            infosDoEvento.preventDefault();
-            onSubmit();
-          }}
-        >
-          {question.alternatives.map((alternative, alternativeIndex) => {
-            const alternativeId = `alternative__${alternativeIndex}`;
-            return (
-              <Card.Topic
-                as="label"
-                htmlFor={alternativeId}
-              >
-                <input
-                                    // style={{ display: 'none' }}
-                  id={alternativeId}
-                  name={questionId}
-                  type="radio"
-                />
-                {alternative}
-              </Card.Topic>
-            );
-          })}
+            <img
+                alt="Descrição"
+                style={{
+                    width: '100%',
+                    height: '150px',
+                    objectFit: 'cover',
+                }}
+                src={question.image}
+            />
+            <Card.Content>
+                <h2>
+                    {question.title}
+                </h2>
+                <p>
+                    {question.description}
+                </p>
 
-          {/* <pre>
-            {JSON.stringify(question, null, 4)}
-          </pre> */}
-          <Button type="submit">
-            Confirmar
-          </Button>
-        </form>
-      </Card.Content>
-    </Card>
-  );
+                <AlternativesForm
+                    onSubmit={(infosDoEvento) => {
+                        infosDoEvento.preventDefault();
+                        setIsQuestionSubmited(true);
+                        setTimeout(() => {
+                            addResult(isCorrect);
+                            onSubmit();
+                            setIsQuestionSubmited(false);
+                            setSelectedAlternative(undefined);
+                        }, 3 * 1000);
+                    }}
+                >
+                    {question.alternatives.map((alternative, alternativeIndex) => {
+                        const alternativeId = `alternative__${alternativeIndex}`;
+                        const alternativeStatus = isCorrect ? 'SUCCESS' : 'ERROR';
+                        const isSelected = selectedAlternative === alternativeIndex;
+                        return (
+                            <Widget.Topic
+                                as="label"
+                                key={alternativeId}
+                                htmlFor={alternativeId}
+                                data-selected={isSelected}
+                                data-status={isQuestionSubmited && alternativeStatus}
+                            >
+                                <input
+                                    style={{ display: 'none' }}
+                                    id={alternativeId}
+                                    name={questionId}
+                                    onChange={() => setSelectedAlternative(alternativeIndex)}
+                                    type="radio"
+                                />
+                                {alternative}
+                            </Widget.Topic>
+                        );
+                    })}
+                    <Button type="submit" disabled={!hasAlternativeSelected}>
+                        Confirmar
+                    </Button>
+                    {isQuestionSubmited && isCorrect && <p>Você acertou!</p>}
+                    {isQuestionSubmited && !isCorrect && <p>Você errou!</p>}
+                </AlternativesForm>
+            </Card.Content>
+        </Card>
+    );
 }
 
 const screenStates = {
-  QUIZ: 'QUIZ',
-  LOADING: 'LOADING',
-  RESULT: 'RESULT',
+    QUIZ: 'QUIZ',
+    LOADING: 'LOADING',
+    RESULT: 'RESULT',
 };
 export default function QuizPage() {
-  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
-  const totalQuestions = db.questions.length;
-  const [currentQuestion, setCurrentQuestion] = React.useState(0);
-  const questionIndex = currentQuestion;
-  const question = db.questions[questionIndex];
+    const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+    const [results, setResults] = React.useState([]);
+    const totalQuestions = db.questions.length;
+    const [currentQuestion, setCurrentQuestion] = React.useState(0);
+    const questionIndex = currentQuestion;
+    const question = db.questions[questionIndex];
 
-  // [React chama de: Efeitos || Effects]
-  // React.useEffect
-  // atualizado === willUpdate
-  // morre === willUnmount
-  React.useEffect(() => {
-    // fetch() ...
-    setTimeout(() => {
-      setScreenState(screenStates.QUIZ);
-    }, 1000);
-    // nasce === didMount
-  }, []);
-
-  function handleSubmitQuiz() {
-    const nextQuestion = questionIndex + 1;
-    if (nextQuestion < totalQuestions) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setScreenState(screenStates.RESULT);
+    function addResult(result) {
+        // results.push(result);
+        setResults([
+            ...results,
+            result,
+        ]);
     }
-  }
 
-  return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        {screenState === screenStates.QUIZ && (
-        <QuestionCard
-          question={question}
-          questionIndex={questionIndex}
-          totalQuestions={totalQuestions}
-          onSubmit={handleSubmitQuiz}
-        />
-        )}
+    // [React chama de: Efeitos || Effects]
+    // React.useEffect
+    // atualizado === willUpdate
+    // morre === willUnmount
+    React.useEffect(() => {
+        // fetch() ...
+        setTimeout(() => {
+            setScreenState(screenStates.QUIZ);
+        }, 1000);
+        // nasce === didMount
+    }, []);
 
-        {screenState === screenStates.LOADING && <LoadingCard />}
+    function handleSubmitQuiz() {
+        const nextQuestion = questionIndex + 1;
+        if (nextQuestion < totalQuestions) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setScreenState(screenStates.RESULT);
+        }
+    }
 
-        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
-      </QuizContainer>
-    </QuizBackground>
-  );
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <QuizLogo />
+                {screenState === screenStates.QUIZ && (
+                    <QuestionWidget
+                        question={question}
+                        questionIndex={questionIndex}
+                        totalQuestions={totalQuestions}
+                        onSubmit={handleSubmitQuiz}
+                        addResult={addResult}
+                    />
+                )}
+
+                {screenState === screenStates.LOADING && <LoadingWidget />}
+
+                {screenState === screenStates.RESULT && <ResultWidget results={results} />}
+            </QuizContainer>
+        </QuizBackground>
+    );
 }
