5d0bc55255e24ce049112e7ab8902899b463265d
Malka <contatolemos@hotmail.com>
2021-01-28 22:35:07 -0300

    Melhorando permissões

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,14 +1,16 @@
 import db from '../../db.json';
 
 export default function dbHandler(request, response) {
-  if (request.method === 'OPTIONS') {
-    response.status(200).end();
+  if (request.method !== 'GET') {
+    response.status(405).json([{
+      'error': 'Não permitido'
+    }]);
     return;
   }
 
   response.setHeader('Access-Control-Allow-Credentials', true);
   response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  response.setHeader('Access-Control-Allow-Methods', 'GET');
 
   response.json(db);
 }
