cf250ae4ba68f2117e2df1f2dbab4a2c3ddf3a29
Malka <contatolemos@hotmail.com>
2021-01-29 21:42:15 -0300

    Finalizando retoques

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,8 +1,21 @@
 import React from 'react';
+import db from '../../db.json';
+import { useRouter } from 'next/router'
 import QuizScreen from "../../src/screen/Quiz";
 import {ThemeProvider} from "styled-components";
+import useNotify from "../../src/useNotify";
 
 export default function QuizDaGaleraPage({dbExterno}) {
+  const router = useRouter();
+  const notify = useNotify();
+  if (dbExterno.theme === undefined || dbExterno.questions === undefined || dbExterno.bg === undefined) {
+    dbExterno = {
+      theme: db.theme,
+      questions: db.questions,
+      bg: db.bg
+    };
+    notify({ title: "Erro com esse Quiz. Tente outro...", type: "error" })
+  }
   return (
     <ThemeProvider theme={dbExterno.theme}>
       <QuizScreen
@@ -15,21 +28,25 @@ export default function QuizDaGaleraPage({dbExterno}) {
 
 export async function getServerSideProps(context) {
   const [projectName, githubUser] = context.query.id.split('___');
-  const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
-    .then((respostaDoServidor) => {
-      if (respostaDoServidor.ok) {
-        return respostaDoServidor.json();
-      }
-      throw new Error('Falha na requisição...');
-    })
-    .then((respostaConvertida) => respostaConvertida)
-    .catch((err) => {
-      console.log(err);
-    });
+  try {
+    const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+      .then((respostaDoServidor) => {
+        if (respostaDoServidor.ok) {
+          return respostaDoServidor.json();
+        }
+        return {};
+      })
+      .then((respostaConvertida) => respostaConvertida)
+      .catch((err) => {
+        return {};
+      });
 
-  return {
-    props: {
-      dbExterno
+    return {
+      props: {
+        dbExterno
+      }
     }
+  } catch (err) {
+    return {};
   }
 }
